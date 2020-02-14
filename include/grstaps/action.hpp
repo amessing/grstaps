55babbe8db0db5ea6550e845b0a58311465022ea
andrew <amessing@gatech.edu>
2020-02-14 14:55:35 -0500

    Placeholder for action stuff. We should try to centralize actions

---
+++ include/grstaps/action.hpp
@@ -0,0 +1,58 @@
+// LICENSE
+
+#ifndef GRSTAPS_DOMAIN_KNOWLEDGE_ACTION_HPP
+#define GRSTAPS_DOMAIN_KNOWLEDGE_ACTION_HPP
+
+// local
+#include "grstaps/noncopyable.hpp"
+
+namespace grstaps
+{
+    /**
+     * Base Action class
+     */
+    class Action : public Noncopyable
+    {
+        public:
+            /**
+             * Default Constructor
+             */
+            Action();
+
+            /**
+             * \returns The identifier for this action
+             */
+            unsigned int id() const;
+
+            /**
+             * \returns Preconditions
+             */
+
+            /**
+             * \returns Effects
+             */
+
+            /**
+             * \returns minimum and maximum duration
+             */
+
+            /**
+             * \returns function for calculating duration
+             */
+
+            /**
+             * \returns The required traits for this action
+             */
+
+        protected:
+            unsigned int m_id; //!< The id of this specific action
+            // condition (equation) - pointer/object/noncopyable
+            // duration min, max (equation?) - pointer/object/noncopyable
+            // effect (equation) - pointer/object/noncopyable
+            // traits/trait mapping
+                
+            static unsigned int s_action_id; //!< The id for the next action that is created
+    };
+}
+
+#endif // GRSTAPS_DOMAIN_KNOWLEDGE_ACTION_HPP
