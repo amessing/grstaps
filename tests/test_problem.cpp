96aa5f3139d76f8da6c6e10bcf425642cfc235e7
andrew <amessing@gatech.edu>
2020-08-29 20:58:44 -0400

    more test problems

--- tests/test_problem.cpp
+++ tests/test_problem.cpp
@@ -34,6 +34,9 @@ namespace grstaps
 {
     namespace test
     {
+        /**
+         * 3 robots start at the source with 3 boxes to bring to the target
+         */
         TEST(Problem, p1)
         {
             Problem problem;
@@ -79,7 +82,7 @@ namespace grstaps
             // Config
             config["mp_boundary_min"]     = 0;
             config["mp_boundary_max"]     = 2;
-            config["mp_query_time"]       = 0.0001;
+            config["mp_query_time"]       = 0.001;
             config["mp_connection_range"] = 0.1;
             problem.setConfig(config);
 
@@ -88,6 +91,304 @@ namespace grstaps
 
             // Save problem
             solver.writeSolution("outputs/p1", solution);
+        }
+
+        /**
+         * 3 robots start at the target and there are 3 boxes at the source to bring to the target
+         */
+        TEST(Problem, p2)
+        {
+            Problem problem;
+
+            std::vector<Location> locations = {Location("source", 0.5, 0.5), Location("target", 1.5, 1.5)};
+            problem.setLocations(locations);
+            problem.setStartingLocations({1, 1, 1});
+
+            std::vector<Problem::TraitVector> robot_traits = {{0.75, 1}, {0.75, 1}, {0.75, 1}};
+            problem.setRobotTraitVector(robot_traits);
+            problem.speedIndex = 1;
+
+            char* domain_filename  = "tests/data/p1/domain.pddl";
+            char* problem_filename = "tests/data/p1/problem.pddl";
+            char* output_filename  = "tests/data/p1/output";
+
+            PlannerParameters parameters;
+
+            parameters.domainFileName  = domain_filename;
+            parameters.problemFileName = problem_filename;
+            parameters.outputFileName  = output_filename;
+            // parameters.generateGroundedDomain = true;
+            // parameters.generateMutexFile = true;
+            // parameters.generateTrace = true;
+            SASTask* task = Setup::doPreprocess(&parameters);
+            problem.setTask(task);
+
+            // All Actions have the same requirements
+            for(unsigned int i = 0; i < task->actions.size(); ++i)
+            {
+                problem.actionToRequirements[task->actions[i].name] = i;
+                problem.actionRequirements.push_back({0.25});
+                problem.actionNonCumRequirements.push_back({0});
+
+                problem.addActionLocation(
+                    task->actions[i].name,
+                    std::make_pair(task->actions[i].name[9] - '1', task->actions[i].name[12] - '1'));
+            }
+
+            // No obstacles
+            nlohmann::json config;
+
+            // Config
+            config["mp_boundary_min"]     = 0;
+            config["mp_boundary_max"]     = 2;
+            config["mp_query_time"]       = 0.001;
+            config["mp_connection_range"] = 0.1;
+            problem.setConfig(config);
+
+            Solver solver;
+            std::shared_ptr<Solution> solution = solver.solve(problem);
+
+            // Save problem
+            solver.writeSolution("outputs/p2", solution);
+        }
+
+        /**
+         * 3 robots start at the target and there are 3 boxes at the source to bring to the target
+         * different robot speeds
+         */
+        TEST(Problem, p3)
+        {
+            Problem problem;
+
+            std::vector<Location> locations = {Location("source", 0.5, 0.5), Location("target", 1.5, 1.5)};
+            problem.setLocations(locations);
+            problem.setStartingLocations({1, 1, 1});
+
+            std::vector<Problem::TraitVector> robot_traits = {{0.75, 3}, {0.75, 2}, {0.75, 1}};
+            problem.setRobotTraitVector(robot_traits);
+            problem.speedIndex = 1;
+
+            char* domain_filename  = "tests/data/p1/domain.pddl";
+            char* problem_filename = "tests/data/p1/problem.pddl";
+            char* output_filename  = "tests/data/p1/output";
+
+            PlannerParameters parameters;
+
+            parameters.domainFileName  = domain_filename;
+            parameters.problemFileName = problem_filename;
+            parameters.outputFileName  = output_filename;
+            // parameters.generateGroundedDomain = true;
+            // parameters.generateMutexFile = true;
+            // parameters.generateTrace = true;
+            SASTask* task = Setup::doPreprocess(&parameters);
+            problem.setTask(task);
+
+            // All Actions have the same requirements
+            for(unsigned int i = 0; i < task->actions.size(); ++i)
+            {
+                problem.actionToRequirements[task->actions[i].name] = i;
+                problem.actionRequirements.push_back({0.25});
+                problem.actionNonCumRequirements.push_back({0});
+
+                problem.addActionLocation(
+                    task->actions[i].name,
+                    std::make_pair(task->actions[i].name[9] - '1', task->actions[i].name[12] - '1'));
+            }
+
+            // No obstacles
+            nlohmann::json config;
+
+            // Config
+            config["mp_boundary_min"]     = 0;
+            config["mp_boundary_max"]     = 2;
+            config["mp_query_time"]       = 0.001;
+            config["mp_connection_range"] = 0.1;
+            problem.setConfig(config);
+
+            Solver solver;
+            std::shared_ptr<Solution> solution = solver.solve(problem);
+
+            // Save problem
+            solver.writeSolution("outputs/p3", solution);
+        }
+
+        /**
+         * 3 robots start at the target and there are 6 boxes at the source to bring to the target
+         */
+        TEST(Problem, p4)
+        {
+            Problem problem;
+
+            std::vector<Location> locations = {Location("source", 0.5, 0.5), Location("target", 1.5, 1.5)};
+            problem.setLocations(locations);
+            problem.setStartingLocations({1, 1, 1});
+
+            std::vector<Problem::TraitVector> robot_traits = {{0.75, 1}, {0.75, 1}, {0.75, 1}};
+            problem.setRobotTraitVector(robot_traits);
+            problem.speedIndex = 1;
+
+            char* domain_filename  = "tests/data/p4/domain.pddl";
+            char* problem_filename = "tests/data/p4/problem.pddl";
+            char* output_filename  = "tests/data/p4/output";
+
+            PlannerParameters parameters;
+
+            parameters.domainFileName  = domain_filename;
+            parameters.problemFileName = problem_filename;
+            parameters.outputFileName  = output_filename;
+            // parameters.generateGroundedDomain = true;
+            // parameters.generateMutexFile = true;
+            // parameters.generateTrace = true;
+            SASTask* task = Setup::doPreprocess(&parameters);
+            problem.setTask(task);
+
+            // All Actions have the same requirements
+            for(unsigned int i = 0; i < task->actions.size(); ++i)
+            {
+                problem.actionToRequirements[task->actions[i].name] = i;
+                problem.actionRequirements.push_back({0.25});
+                problem.actionNonCumRequirements.push_back({0});
+
+                problem.addActionLocation(
+                    task->actions[i].name,
+                    std::make_pair(task->actions[i].name[9] - '1', task->actions[i].name[12] - '1'));
+            }
+
+            // No obstacles
+            nlohmann::json config;
+
+            // Config
+            config["mp_boundary_min"]     = 0;
+            config["mp_boundary_max"]     = 2;
+            config["mp_query_time"]       = 0.001;
+            config["mp_connection_range"] = 0.1;
+            problem.setConfig(config);
+
+            Solver solver;
+            std::shared_ptr<Solution> solution = solver.solve(problem);
+
+            // Save problem
+            solver.writeSolution("outputs/p4", solution);
+        }
+
+        /**
+         * 3 robots start at the target and there are 6 boxes at the source to bring to the target
+         * Differt speeds
+         */
+        TEST(Problem, p5)
+        {
+            Problem problem;
+
+            std::vector<Location> locations = {Location("source", 0.5, 0.5), Location("target", 1.5, 1.5)};
+            problem.setLocations(locations);
+            problem.setStartingLocations({1, 1, 1});
+
+            std::vector<Problem::TraitVector> robot_traits = {{0.75, 3}, {0.75, 2}, {0.75, 1}};
+            problem.setRobotTraitVector(robot_traits);
+            problem.speedIndex = 1;
+
+            char* domain_filename  = "tests/data/p4/domain.pddl";
+            char* problem_filename = "tests/data/p4/problem.pddl";
+            char* output_filename  = "tests/data/p4/output";
+
+            PlannerParameters parameters;
+
+            parameters.domainFileName  = domain_filename;
+            parameters.problemFileName = problem_filename;
+            parameters.outputFileName  = output_filename;
+            // parameters.generateGroundedDomain = true;
+            // parameters.generateMutexFile = true;
+            // parameters.generateTrace = true;
+            SASTask* task = Setup::doPreprocess(&parameters);
+            problem.setTask(task);
+
+            // All Actions have the same requirements
+            for(unsigned int i = 0; i < task->actions.size(); ++i)
+            {
+                problem.actionToRequirements[task->actions[i].name] = i;
+                problem.actionRequirements.push_back({0.25});
+                problem.actionNonCumRequirements.push_back({0});
+
+                problem.addActionLocation(
+                    task->actions[i].name,
+                    std::make_pair(task->actions[i].name[9] - '1', task->actions[i].name[12] - '1'));
+            }
+
+            // No obstacles
+            nlohmann::json config;
+
+            // Config
+            config["mp_boundary_min"]     = 0;
+            config["mp_boundary_max"]     = 2;
+            config["mp_query_time"]       = 0.001;
+            config["mp_connection_range"] = 0.1;
+            problem.setConfig(config);
+
+            Solver solver;
+            std::shared_ptr<Solution> solution = solver.solve(problem);
+
+            // Save problem
+            solver.writeSolution("outputs/p5", solution);
+        }
+
+        /**
+         * 3 robots start at the target and there are 3 boxes at the source to bring to the target
+         * boxes require 2 robots to carry
+         */
+        TEST(Problem, p6)
+        {
+            Problem problem;
+
+            std::vector<Location> locations = {Location("source", 0.5, 0.5), Location("target", 1.5, 1.5)};
+            problem.setLocations(locations);
+            problem.setStartingLocations({1, 1, 1});
+
+            std::vector<Problem::TraitVector> robot_traits = {{0.25, 1}, {0.25, 1}, {0.25, 1}};
+            problem.setRobotTraitVector(robot_traits);
+            problem.speedIndex = 1;
+
+            char* domain_filename  = "tests/data/p4/domain.pddl";
+            char* problem_filename = "tests/data/p4/problem.pddl";
+            char* output_filename  = "tests/data/p4/output";
+
+            PlannerParameters parameters;
+
+            parameters.domainFileName  = domain_filename;
+            parameters.problemFileName = problem_filename;
+            parameters.outputFileName  = output_filename;
+            // parameters.generateGroundedDomain = true;
+            // parameters.generateMutexFile = true;
+            // parameters.generateTrace = true;
+            SASTask* task = Setup::doPreprocess(&parameters);
+            problem.setTask(task);
+
+            // All Actions have the same requirements
+            for(unsigned int i = 0; i < task->actions.size(); ++i)
+            {
+                problem.actionToRequirements[task->actions[i].name] = i;
+                problem.actionRequirements.push_back({0.4});
+                problem.actionNonCumRequirements.push_back({0});
+
+                problem.addActionLocation(
+                    task->actions[i].name,
+                    std::make_pair(task->actions[i].name[9] - '1', task->actions[i].name[12] - '1'));
+            }
+
+            // No obstacles
+            nlohmann::json config;
+
+            // Config
+            config["mp_boundary_min"]     = 0;
+            config["mp_boundary_max"]     = 2;
+            config["mp_query_time"]       = 0.001;
+            config["mp_connection_range"] = 0.1;
+            problem.setConfig(config);
+
+            Solver solver;
+            std::shared_ptr<Solution> solution = solver.solve(problem);
+
+            // Save problem
+            solver.writeSolution("outputs/p6", solution);
         }
     }  // namespace test
 }  // namespace grstaps
