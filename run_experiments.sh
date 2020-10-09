cbac7665e5962892f8606758ff31275d25f6afe6
andrew <amessing@gatech.edu>
2020-10-09 16:18:02 -0400

    updated experiment script

--- run_experiments.sh
+++ run_experiments.sh
@@ -13,12 +13,38 @@ ulimit -v 17179869184
 
 for i in {1..200}
 do
+  echo "Problem ${i}"
+  if [ -f "problems/problem_${i}.json" ]; then
+    echo "Already done"
+    continue
+  fi
+
+  j=$(( ( i + 1 ) % 5 ))
+  echo "Map ${j}"
+  if [ "${j}" == "3" ]; then
+    echo "Ignore"
+    continue
+  fi
+  echo "timeout 30m ./icra_experiments -p ${i} -s"
+  eval "timeout 30m ./icra_experiments -p ${i} -s"
+done
+
+for a in "0.0" "0.25" "0.5" "0.75" "1.0"
+do
+  for i in {1..200}
+  do
     echo "Problem ${i}"
-    eval "timeout 30m ./icra_experiments -p ${i} -a 0.0"
-    eval "timeout 30m ./icra_experiments -p ${i} -a 0.25"
-    eval "timeout 30m ./icra_experiments -p ${i} -a 0.5"
-    eval "timeout 30m ./icra_experiments -p ${i} -a 0.75"
-    eval "timeout 30m ./icra_experiments -p ${i} -a 1.0"
-    #eval "timeout 30m ./icra_experiments -p ${i} -s"
+    j=$(( ( i + 1 ) % 5 ))
+    echo "Map ${j}"
+    if [ "${j}" == "3" ]; then
+      echo "Ignore"
+      continue
+    fi
+
+    if [ ! -f "outputs/output_${i}_${a}_0.json" ]; then
+      echo "timeout 30m ./icra_experiments -p ${i} -a ${a}"
+      eval "timeout 30m ./icra_experiments -p ${i} -a ${a}"
+    fi
+  done
 done
 
