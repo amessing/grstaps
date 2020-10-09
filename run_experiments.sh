2af465e1ef856a94a08a15060166d24cd6d14b62
Andrew Messing <amessing@gatech.edu>
2020-10-09 09:37:30 -0400

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -14,11 +14,11 @@ ulimit -v 17179869184
 for i in {1..200}
 do
     echo "Problem ${i}"
-    #eval "timeout 30m ./icra_experiments -p ${i} -a 0.0"
-    #eval "timeout 30m ./icra_experiments -p ${i} -a 0.25"
-    #eval "timeout 30m ./icra_experiments -p ${i} -a 0.5"
-    #eval "timeout 30m ./icra_experiments -p ${i} -a 0.75"
-    #eval "timeout 30m ./icra_experiments -p ${i} -a 1.0"
-    eval "timeout 30m ./icra_experiments -p ${i} -s"
+    eval "timeout 30m ./icra_experiments -p ${i} -a 0.0"
+    eval "timeout 30m ./icra_experiments -p ${i} -a 0.25"
+    eval "timeout 30m ./icra_experiments -p ${i} -a 0.5"
+    eval "timeout 30m ./icra_experiments -p ${i} -a 0.75"
+    eval "timeout 30m ./icra_experiments -p ${i} -a 1.0"
+    #eval "timeout 30m ./icra_experiments -p ${i} -s"
 done
 
