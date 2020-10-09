30eeea47ceff4deb8f79775d0d01dd3d1815bfd2
glenneville <gneville6@gatech.edu>
2020-10-09 12:32:43 -0400

    updated map 5 and set the other experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -14,6 +14,11 @@ ulimit -v 17179869184
 for i in {1..200}
 do
     echo "Problem ${i}"
+    if [ ! -f "problems/problem_${i}.json" ]; then
+       # echo "problem ${i} not found" 
+       eval "timeout 30m ./icra_experiments -p ${i} -s"
+    fi
+
     eval "timeout 30m ./icra_experiments -p ${i} -a 0.0"
     eval "timeout 30m ./icra_experiments -p ${i} -a 0.25"
     eval "timeout 30m ./icra_experiments -p ${i} -a 0.5"
