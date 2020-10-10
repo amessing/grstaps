348720fda9a9ddc52eced9d757d2661b720a7e3f
gneville6 <gneville6@gatech.edu>
2020-10-10 09:27:30 -0400

    Fixes #13 add motion plan times to schedule properly

--- include/grstaps/Scheduling/tabu.h
+++ include/grstaps/Scheduling/tabu.h
@@ -6,8 +6,8 @@
 #include <../lib/unordered_map/robin_hood.h>
 #include <grstaps/Scheduling/Scheduler.h>
 
-#define TABU_LENGTH 2000
-#define NUM_INTERATION 2
+#define TABU_LENGTH 200
+#define NUM_INTERATION 1
 #define TIME_TRY 10
 #define QUALITY_THRESHOLD 1.3
 
